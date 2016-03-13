c024f44a975d3c8f34a9c92cbec24afe0eb4144a
Hugo P <hugo.pham@hotmail.com>
2016-03-13 17:02:48 -0400

    Added files via upload

---
+++ PongLibrary/Ball.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace PongLibrary
+{
+    public class Ball
+    {
+        private Vector2 velocity;
+        private Rectangle screenDimensions;
+        private Rectangle boundingBox;
+        private Paddle paddle; 
+        public Ball(int ballWidth, int ballHeight,
+            int screenWidth, int screenHeight, Vector2 velocity, Paddle paddle)
+        {
+            this.velocity = velocity;
+            this.paddle = paddle;
+            screenDimensions.Width = screenWidth;
+            screenDimensions.Height = screenHeight;
+            boundingBox.Width = ballWidth;
+            boundingBox.Height = ballHeight;
+            boundingBox.X = (screenWidth - ballWidth) / 2;
+            boundingBox.Y = 0;
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                return boundingBox;
+            }
+        }
+
+        public void Move()
+        {
+            boundingBox.Y += (int)velocity.Y;
+            boundingBox.X += (int)velocity.X;
+
+            this.Bounce();
+            //boundingBox.X += (int) (MathHelper.Clamp(boundingBox.X + velocity.X, 0, screenDimensions.X - boundingBox.Width));
+            //boundingBox.X += (int)velocity.X;
+            //boundingBox.Y += (int)(MathHelper.Clamp(boundingBox.Y + velocity.Y, 0, screenDimensions.Height - boundingBox.Height));
+            
+            
+            
+            
+            
+        }
+
+        public void Bounce()
+        {
+            if (this.boundingBox.Intersects(paddle.BoundingBox))
+                velocity.Y *= -1;
+
+
+            if (boundingBox.Y >= screenDimensions.Height - boundingBox.Height)
+            {
+                velocity.Y *= 0;
+                velocity.X *= 0;
+                // velocity.Y *= -1;
+            }
+
+            if (boundingBox.Y <= 0)
+            {
+                velocity.Y *= -1;
+            }
+
+            if (boundingBox.X >= screenDimensions.Width - boundingBox.Width)
+                velocity.X *= -1;
+
+            if (boundingBox.X <= 0)
+                velocity.X *= -1;
+
+        }
+
+
+
+    }
+}
---
+++ PongLibrary/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace PongLibrary
+{
+    public class Paddle
+    {
+        private readonly int speed;
+        private int screenWidth;
+        private Rectangle boundingBox;
+
+        public Paddle(int paddleWidth, int paddleHeight,
+            int screenWidth, int screenHeight, int speed)
+        {
+            this.speed = speed;
+            this.screenWidth = screenWidth;
+            boundingBox.Width = paddleWidth;
+            boundingBox.Height = paddleHeight;
+            boundingBox.X = (screenWidth - paddleWidth) / 2;
+            boundingBox.Y = screenHeight - paddleHeight;
+        }
+
+        public Rectangle BoundingBox { 
+            get
+            {
+                return boundingBox;
+            }
+        }
+
+        public void MoveLeft()
+        {         
+            boundingBox.X = MathHelper.Clamp(boundingBox.X - speed, 0, screenWidth - boundingBox.Width);
+        } 
+
+        public void MoveRight()
+        {
+            boundingBox.X = MathHelper.Clamp(boundingBox.X + speed, 0, screenWidth - boundingBox.Width);
+        } 
+    }
+}
